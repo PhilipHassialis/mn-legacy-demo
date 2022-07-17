30a674f1c2c8e1ca90158c43f39a636e0ed9bf16
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-17 14:32:37 +0000

    Initial commit

---
+++ src/test/java/com/philip/MnLegacyDemoTest.java
@@ -0,0 +1,21 @@
+package com.philip;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MnLegacyDemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
