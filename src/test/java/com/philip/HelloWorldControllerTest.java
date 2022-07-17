36adad307309043b0bf15b143ccda9ba2700cfeb
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-17 17:55:20 +0300

    First Micronaut test experience

--- src/test/java/com/philip/HelloWorldControllerTest.java
+++ src/test/java/com/philip/HelloWorldControllerTest.java
@@ -1,21 +1,35 @@
 package com.philip;
 
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javax.inject.Inject;
 
 @MicronautTest
-class MnLegacyDemoTest {
+class HelloWorldControllerTest {
 
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testHelloResponse() {
+        final String result = client.toBlocking().retrieve("/hello");
+        assertEquals("Hello World", result);
     }
 
 }
