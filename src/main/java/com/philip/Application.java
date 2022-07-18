6bf852b55dea3089e241460d4e3302616465d9d8
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-18 09:18:48 +0300

    Logging

--- src/main/java/com/philip/Application.java
+++ src/main/java/com/philip/Application.java
@@ -1,14 +1,19 @@
 package com.philip;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         final ApplicationContext context = Micronaut.run(Application.class, args);
         final HelloWorldService service = context.getBean(HelloWorldService.class);
-        System.out.println(service.sayHi());
+        LOG.info(service.sayHi());
 
     }
 }
--- src/main/java/com/philip/HelloWorldService.java
+++ src/main/java/com/philip/HelloWorldService.java
@@ -2,12 +2,24 @@ package com.philip;
 
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.context.annotation.Property;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
 
 // import io.micronaut.context.annotation.Value;
 
 @Singleton
 public class HelloWorldService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+  @EventListener
+  public void onStartup(StartupEvent startupEvent) {
+    LOG.debug("Startup {}", startupEvent);
+  }
 
   @Property(name = "hello.service.greeting", defaultValue = "default greeting")
   private String greeting;
