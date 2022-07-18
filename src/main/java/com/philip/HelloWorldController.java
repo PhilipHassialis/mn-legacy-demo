5a615acea1fe685eb173008f88f7cfa5c1184932
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-18 09:03:38 +0300

    Application configuration

--- src/main/java/com/philip/HelloWorldController.java
+++ src/main/java/com/philip/HelloWorldController.java
@@ -1,11 +1,11 @@
 package com.philip;
 
-import javax.inject.Inject;
+// import javax.inject.Inject;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
-@Controller("/hello")
+@Controller("${hello.controller.path}")
 public class HelloWorldController {
 
   // @Inject
--- src/main/java/com/philip/HelloWorldService.java
+++ src/main/java/com/philip/HelloWorldService.java
@@ -2,9 +2,17 @@ package com.philip;
 
 import javax.inject.Singleton;
 
+import io.micronaut.context.annotation.Property;
+
+// import io.micronaut.context.annotation.Value;
+
 @Singleton
 public class HelloWorldService {
+
+  @Property(name = "hello.service.greeting", defaultValue = "default greeting")
+  private String greeting;
+
   public String sayHi() {
-    return "Hello from service";
+    return greeting;
   }
 }
